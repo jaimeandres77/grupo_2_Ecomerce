6ad41820f7fef2be76ac46a53471815dfc59172e
Fabian Ricardo Forero Gutierrez <fabian.400@outlook.com>
2021-12-12 21:47:02 -0500

    Creación de lo

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.use(express.static(path.resolve(__dirname,'./public')));
 app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')));
+
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')));
