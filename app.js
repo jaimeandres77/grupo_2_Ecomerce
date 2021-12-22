d00a26e5b9c7c1865d2e4a5405ff330a61d6b1bf
Fabian Forero <fabian.400@outlook.com>
2021-12-22 11:05:54 -0500

    [UPDATE] Corrigiendo puerto de escucha

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname,'./public')));
-app.listen(3001,() => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
 app.get('/productdetail',(req,res) => res.sendFile(path.resolve(__dirname,'./views/productDetail.html')));
