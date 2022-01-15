e6aad30feb1169598cc8ba55e2bc6e096a4ac9f9
Johan2511 <johanpenuelah@gmail.com>
2022-01-14 22:52:26 -0500

    Se agrego las rutas register y login en mainRouter, los render en mainController y  cambio de las extensiones del mismo nombre a ejs

--- src/Controller/mainController.js
+++ src/Controller/mainController.js
@@ -1,5 +1,12 @@
 module.exports={
     index: (req, res) => {
         res.render('home');
-    }
+    },
+    register: (req, res) => {
+        res.render('register');
+    },
+    login: (req, res) => {
+        res.render('login');
+    },
+    
 }
--- src/Routes/mainRouter.js
+++ src/Routes/mainRouter.js
@@ -3,4 +3,11 @@ const router = express.Router();
 const mainController = require('../Controller/mainController');
 
 router.get('/', mainController.index);
+
+router.get('/register', mainController.register);
+
+router.get('/login', mainController.login);
+
+
+
 module.exports= router;
