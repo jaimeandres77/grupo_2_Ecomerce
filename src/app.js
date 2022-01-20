66729b00d1987fc885bfe8fe2451883c8b22ace8
Johan2511 <johanpenuelah@gmail.com>
2022-01-20 14:33:56 -0500

    Modificacion del register y login, eliminado las rutas login y register de app

--- src/app.js
+++ src/app.js
@@ -11,10 +11,5 @@ app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 
-
-app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')));
-
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')));
-
 app.get('/productdetail',(req,res) => res.sendFile(path.resolve(__dirname,'./views/productDetail.html')));
 
