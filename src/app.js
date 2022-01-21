b9ca84b6b7e7321ef14a020d8055e8f3aaa57b42
Fabián <fabian.400@outlook.com>
2022-01-20 20:06:52 -0500

    [ADD] Sprint 4 agregando configuracion y dependencias de method-override y multer

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 const mainRouter = require('./Routes/mainRouter');
 const productRouter = require('./Routes/productRouter');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
-app.use('/', mainRouter);
-app.use('/product', productRouter);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
-app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 
-app.get('/productdetail',(req,res) => res.sendFile(path.resolve(__dirname,'./views/productDetail.html')));
+app.use('/', mainRouter);
+app.use('/product', productRouter);
 
+app.listen(port,() => console.log(`Servidor corriendo en el puerto ${port}`));
