bf4f0f971481c06c84587ef6a27f23361075cb5c
Jaime Andres <jaime.artunduaga@uao.edu.co>
2022-02-11 09:20:22 -0500

    Validaciones createProduct

--- src/Controller/productController.js
+++ src/Controller/productController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { validationResult } = require('express-validator');
 
 const productsFilePath = path.resolve(__dirname,'../databases/productos.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath,'utf-8'));
@@ -12,12 +13,17 @@ module.exports = {
         res.render('product/createProduct');
     },
     createSend: (req,res) =>{
-        console.log({data: req.body, file: req.file});
+        const errors = validationResult(req);
+        if(errors.errors.length > 0){
+            console.log({oldData: req.body});
+            return res.render('product/createProduct',{errors: errors.mapped(), oldData: req.body});
+        }
         const id = products[products.length - 1].id + 1 || 1;
+        const imagen = req.file?.filename !== undefined ? req.file.filename : 'defaultProduct.jpg';
         const newProduct = {
             id: id,
             ... req.body,
-            imagen: req.file.filename,
+            imagen: imagen,
             descuento: 0,
             stock: 0,
             ventas: 0,
--- src/Routes/productRouter.js
+++ src/Routes/productRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const productController = require('../Controller/productController');
+const {body,check} = require('express-validator');
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -13,13 +14,19 @@ const storage = multer.diskStorage({
     }
 });
 
+const validateCreateForm = [
+    body('nombre').not().isEmpty().withMessage('Debes rellenar el campo del Nombre del producto'),
+    body('precio').not().isEmpty().withMessage('Debes asignarle un precio al producto'),
+    body('sku').not().isEmpty().withMessage('Debes asignarle un sku al producto'),
+]
+
 const upload = multer({storage})
 
 router.get('/detail',productController.detail);
 
 router.get('/create',productController.create);
 
-router.post('/create', upload.single('imagen') , productController.createSend);
+router.post('/create', upload.single('imagen'),validateCreateForm, productController.createSend);
 
 router.get('/edit/:id', productController.edit);
 
