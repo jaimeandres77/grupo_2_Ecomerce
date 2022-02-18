b30ec8cc62a82af7d9e1829111cd50f30f40edc1
Fabián <fabian.400@outlook.com>
2022-02-18 17:13:09 -0500

    Agregando validacion al inicio de sesion

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -9,20 +9,21 @@ module.exports = {
   },
   processRegister: (req,res) => {
     const resultValidation = validationResult(req);
-    const email = User.findByField('email',req.body.email);
-    const password = req.body.password === req.body.password_repeat && req.body.password.length > 0 ? false : true;
-    if (resultValidation.errors.length > 0 || email || password) {
-      if(email){
-        if(password){
-          return res.render('user/register',{errors: resultValidation.mapped(),errors: {email: {msg: 'Correo ya existente'},password: {msg: 'Las contraseñas no son iguales'}},oldData: req.body});
-        }
-        return res.render('user/register',{errors: resultValidation.mapped(),errors: {email: {msg: 'Correo ya existente'}},oldData: req.body});
-      }
-      if(password){
-        return res.render('user/register',{errors: resultValidation.mapped(),errors: {password: {msg: 'Las contraseñas no son iguales'}},oldData: req.body});
-      }
+    if (resultValidation.errors.length > 0) {
       return res.render('user/register',{errors: resultValidation.mapped(),oldData: req.body});
     }
+    const email = User.findByField('email',req.body.email);
+    const password = req.body.password === req.body.password_repeat && req.body.password.length > 0 ? false : true;
+    if(email){
+      if(password){
+        return res.render('user/register',{errors: resultValidation.mapped(),errors: {email: {msg: 'Correo ya existente'},password: {msg: 'Las contraseñas no son iguales'}},oldData: req.body});
+      }
+      return res.render('user/register',{errors: resultValidation.mapped(),errors: {email: {msg: 'Correo ya existente'}},oldData: req.body});
+    }
+    if(password){
+      return res.render('user/register',{errors: resultValidation.mapped(),errors: {password: {msg: 'Las contraseñas no son iguales'}},oldData: req.body});
+    }
+
     delete req.body.password_repeat;
     const newUser = {
       ... req.body,
@@ -33,10 +34,15 @@ module.exports = {
     return res.redirect('/user/login');
   },
   login: (req, res) => {
-      res.render('user/login');
+    res.render('user/login');
   },
   loginProcess: (req,res) => {
-    res.send('Procesando...')
+    const userToLogin = User.findByField('email',req.body.email);
+    const isOkThePassword = req.body.password !== undefined ? bcrypt.compareSync(req.body.password,userToLogin.password) : false;
+    if(userToLogin && isOkThePassword){
+      res.send('Ok');
+    }
+    res.render('user/login',{errors: {email: {msg: 'No se encuentra este email en nuestra Base de Datos o la contraseña es incorrecta'}}, oldData: req.body});
   }
 }
 
