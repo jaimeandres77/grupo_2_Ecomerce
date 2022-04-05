d416f854ec5943b3c28c0ab5bbf2d9039666cea9
Fabián <fabian.400@outlook.com>
2022-04-05 16:11:09 -0500

    Agregando validacion del formulario de juegos por parte del frontend

--- src/Controller/mainController.js
+++ src/Controller/mainController.js
@@ -5,7 +5,7 @@ module.exports={
         try {
             const limit = 10;
             const count = await db.Games.count();
-            const page = isNaN(req.query.page) || parseInt(req.query.page) <= 1 || parseInt(req.query.page) > parseInt(count / limit) ? 1 : parseInt(req.query.page);
+            const page = isNaN(req.query.page) || parseInt(req.query.page) <= 1 || parseInt(req.query.page) > Math.ceil(count / limit) ? 1 : parseInt(req.query.page);
             const games = await db.Games.findAll({attributes: ['id','name','price','image'],order: [['name','ASC']],limit,offset: (page - 1) * 10});
             res.render('home',{games,count,page,limit});
         } catch (error) {
